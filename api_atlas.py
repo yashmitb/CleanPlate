040d81fc6e879fc49c3d46ca351845cd1e1ca8a2
A-Choudhari <akshat.reach@gmail.com>
2026-02-01 10:55:59 -0800

    changed cor permissions

--- api_atlas.py
+++ api_atlas.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 from user_preference_manager import UserFoodPreferenceManager
 import json
 import os
@@ -10,6 +11,7 @@ import services
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)  # This enables CORS for all routes and all origins
 
 # --- Documentation ---
 @app.route('/')
